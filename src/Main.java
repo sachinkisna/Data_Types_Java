e601051c5165744220dc25a55e21b35667032dff
Sachinkisna <sachinsoftware0001@gmail.com>
2024-11-11 15:28:48 +0530

    aded

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Below are the all dta types listed below");
+
     }
 }
